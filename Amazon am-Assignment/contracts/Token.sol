700b08d086f2c4e1811c792bd7ed379e8e9f902a
Kartik <92619240+Kartik42890@users.noreply.github.com>
2023-08-29 23:23:26 +0530

    Update Token.sol

--- Amazon am-Assignment/contracts/Token.sol
+++ Amazon am-Assignment/contracts/Token.sol
@@ -6,9 +6,19 @@ import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract Token is ERC20, ERC20Burnable, Ownable {
-    constructor() ERC20("Amazon", "Am") {}
+    constructor() ERC20("Rizzlers", "RIZZ") {}
 
     function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
+    }
+
+    // This function allows the owner to burn a specific amount of their own tokens
+    function burnFromOwner(uint256 amount) public onlyOwner {
+        _burn(owner(), amount);
+    }
+
+    // This function allows any holder to transfer tokens to another address
+    function transferTokens(address to, uint256 amount) public {
+        _transfer(msg.sender, to, amount);
     }
 }
